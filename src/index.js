05a5b6bc9fff2c7f9cf85d99b48e700ddd5e8e9e
Samanimkr <samanimkr@outlook.com>
2018-10-18 14:03:53 +0100

    Setup redux and deleted default files from create-react-app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+
+const store = createStore();
+
+export class App extends React.Component {
+  render(){
+    return (
+      <h1>Learning Redux</h1>
+    )
+  }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
