f91a3affd72bf3071e68fe67db4d783754dcef2c
Samanimkr <samanimkr@outlook.com>
2018-10-22 12:41:08 +0100

    Integrating redux with react

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducers/rootReducer';
 
 
-const initState = {
-    todos: [],
-    posts: []
-}
-
-function myReducer(state = initState, action){
-    if (action.type == 'ADD_TODO'){
-        return {
-            ...state,
-            todos: [...state.todos, action.todo]
-        }
-    }
-    if (action.type == 'ADD_POST'){
-        return {
-            ...state,
-            posts: [...state.posts, action.post]
-        }
-    }
-}
-
-const store = createStore(myReducer);
-
-store.subscribe(() => {
-    console.log('state updated');
-    console.log(store.getState());
-})
+const store = createStore(rootReducer);
 
 
-store.dispatch({ type: 'ADD_TODO', todo: 'buy milk ' });
-store.dispatch({ type: 'ADD_TODO', todo: 'sleep some more' });
-store.dispatch({ type: 'ADD_POST', post: 'Best post ever' });
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
